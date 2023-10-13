5f7390ad9c34fa8ed1a7c99de10f802b55f25676
Roman Jurkovič <98027196+Knight20102010@users.noreply.github.com>
2023-10-13 11:45:10 +0200

    vytvorenie premmenej

---
+++ 2_Zaklady.py
@@ -0,0 +1,12 @@
+print("Getting to know you!")
+MyName = input("Whats your fucking name: ")
+Food = input("Whats your favourite food: ")
+Music = input(("Whats your favourite music?:"))
+Place = input("Where do you live?: ")
+print("You are ",MyName)
+print("You are probably hungry for ",Food)
+print("and you are definitely getting your groove to ", Music)
+print("living in the amazing",Place)
+
+r=78
+like="roman"
